747e961350d7689414f48ceb1a49cefaaf3399a7
Chrysanthemum Lovelace <chryslovelace@gmail.com>
2025-03-20 13:50:23 -0400

    update actions to pull data from taxonomy term field

--- src/Plugin/Action/GenerateTranskribusHOCRDerivative.php
+++ src/Plugin/Action/GenerateTranskribusHOCRDerivative.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\transkribus_derivative\Plugin\Action;
 
+use Drupal\Core\Entity\EntityInterface
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\islandora\Plugin\Action\AbstractGenerateDerivative;
 
@@ -23,19 +24,41 @@ class GenerateTranskribusHOCRDerivative extends AbstractGenerateDerivative {
         $config['queue'] = 'islandora-connector-transkribus';
         $config['destination_media_type'] = 'extracted_text';
         $config['scheme'] = 'fedora';
-        $config['htrid'] = '';
+        $config['transkribus_model_field'] = '';
         return $config;
     }
 
     public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
         $form = parent::buildConfigurationForm($form, $form_state);
-        $form['args']['#description'] = $this->t("Additional arguments to send to Transkribus microservice");
-        $form['htrid'] = [
-            '#title' => $this->t("HTR ID"),
-            '#type' => 'textfield',
+        unset($form['args']);
+
+        $taxonomy_fields = Drupal::entityTypeManager()->getStorage('field_storage_config')->loadByProperties([
+            'type' => 'entity_reference',
+            'settings' => [ 'target_type' => 'taxonomy_term' ],
+        ]);
+
+        $field_options = array_merge(['' => ''], array_combine(array_keys($taxonomy_fields), array_keys($taxonomy_fields)));
+
+        $form['transkribus_model_field'] = [
+            '#title' => $this->t("Transkribus Model Field"),
+            '#type' => 'select',
+            '#options' => $field_options,
             '#required' => true,
-            '#description' => $this->t("ID of Transkribus HTR model")
+            '#description' => $this->t("Entity field containing a reference to the Transkribus Model taxonomy term"),
+            '#default_value' => $this->configuration['transkribus_model_field']
         ];
         return $form;
+    }
+
+    protected function generateData(EntityInterface $entity) {
+        $data = parent::generateData($entity);
+        $model = $entity->get($this->configuration['transkribus_model_field'])->entity;
+        $data['args'] = 'page --htrid=' . $model->field_htr_model_id->value;
+        return $data;
+    }
+
+    public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+        parent::submitConfigurationForm($form, $form_state);
+        $this->configuration['transkribus_model_field'] = $form_state->getValue('transkribus_model_field');
     }
 }
