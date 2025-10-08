b4ec01a4ab32075fa70a10438b63f40a1b62b51c
Chrysanthemum Lovelace <chryslovelace@gmail.com>
2025-10-08 10:52:01 -0400

    typo fix and better defaults

--- src/Plugin/Action/GenerateTranskribusHOCRDerivativeFile.php
+++ src/Plugin/Action/GenerateTranskribusHOCRDerivativeFile.php
@@ -15,18 +15,25 @@ use Drupal\islandora\Exception\IslandoraDerivativeException;
  * )
  */
 class GenerateTranskribusHOCRDerivativeFile extends AbstractGenerateDerivativeMediaFile {
+
+    /**
+     * {@inheritdoc}
+     */
     public function defaultConfiguration() {
         $config = parent::defaultConfiguration();
         $config['path'] = '[date:custom:Y]-[date:custom:m]/[node:nid]-[term:name].shtml';
         $config['event'] = 'Generate Transkribus HOCR Derivative';
         $config['source_term_uri'] = 'http://pcdm.org/use#OriginalFile';
-        $config['derivative_term_uri'] = 'http://pcdm.org/use#ExtractedText';
+        $config['derivative_term_uri'] = 'https://discoverygarden.ca/use#hocr';
         $config['mimetype'] = 'text/html';
         $config['queue'] = 'islandora-connector-transkribus';
         $config['transkribus_model_field'] = '';
         return $config;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
         $form = parent::buildConfigurationForm($form, $form_state);
         unset($form['args']);
@@ -49,6 +56,9 @@ class GenerateTranskribusHOCRDerivativeFile extends AbstractGenerateDerivativeMe
         return $form;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function generateData(EntityInterface $entity) {
         $data = parent::generateData($entity);
         $model_field_path = explode('.', $this->configuration['transkribus_model_field']);
@@ -60,27 +70,30 @@ class GenerateTranskribusHOCRDerivativeFile extends AbstractGenerateDerivativeMe
         $data['args'] = 'page --htrid=' . $model->get('field_htr_model_id')->getString();
         $accuracy_threshold = $model->get('field_accuracy_threshold');
         if (!$accuracy_threshold->isEmpty()) {
-            $data['args'] .= ' baselineaccuracythreshold=' . $accuracy_threshold->getString();
+            $data['args'] .= ' --baselineaccuracythreshold=' . $accuracy_threshold->getString();
         }
         $line_detection_model = $model->get('field_line_detection_model_id');
         if (!$line_detection_model->isEmpty()) {
-            $data['args'] .= ' linedetectionmodelid=' . $line_detection_model->getString();
+            $data['args'] .= ' --linedetectionmodelid=' . $line_detection_model->getString();
         }
         $max_dist_merging = $model->get('field_max_dist_for_merging');
         if (!$max_dist_merging->isEmpty()) {
-            $data['args'] .= ' maxdistformerging=' . $max_dist_merging->getString();
+            $data['args'] .= ' --maxdistformerging=' . $max_dist_merging->getString();
         }
         $min_baseline_length = $model->get('field_minimal_baseline_length');
         if (!$min_baseline_length->isEmpty()) {
-            $data['args'] .= ' minimalbaselinelength=' . $min_baseline_length->getString(); 
+            $data['args'] .= ' --minimalbaselinelength=' . $min_baseline_length->getString(); 
         }
         $num_text_regions = $model->get('field_num_text_regions');
         if (!$num_text_regions->isEmpty()) {
-            $data['args'] .= ' numtextregions=' . $num_text_regions->getString();
+            $data['args'] .= ' --numtextregions=' . $num_text_regions->getString();
         }
         return $data;
     }
     
+    /**
+     * {@inheritdoc}
+     */
     public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
         parent::submitConfigurationForm($form, $form_state);
         $this->configuration['transkribus_model_field'] = $form_state->getValue('transkribus_model_field');
